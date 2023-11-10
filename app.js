44a680ce5e7b86680216ffc6efe7783a9e829b0f
WillyWG <willywg@gmail.com>
2023-11-10 17:35:42 -0500

    feat(app.js): add support for streaming chat messages by room ID
    - Added a new route `/stream-chat/:room_id` to handle streaming chat messages.
    - Set the response header `Content-Type` to indicate that the response is a stream of events.
    - Modified the `app.post('/chat')` route to include the user message as an object with `role` and `content` properties.
    - Retrieve existing messages from the database for the specified `room_id` and append them to the `messages` array.
    - Insert the user message into the database with the specified `room_id`.
    - Use the `messages` array as the value for the `messages` property when calling `openai.chat.completions.create()`.
    - Iterate over the response from OpenAI and check if the chat is finished. If it is, send an end event to the client and insert the assistant's response into the database.
    - Concatenate the assistant's response chunks to form the full assistant response.
    - Insert the full assistant response into the database.
    - Removed console.log statements.
    
    feat(asistente.html): update event source URL to include room ID
    
    - Added a `roomId` variable with a hardcoded value of '123'.
    - Updated the event source URL to include the `roomId` parameter.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const openAI = require('openai');
 const path = require('path');
+const db = require('./db');
 require('dotenv').config();
 
 
@@ -46,30 +47,61 @@ app.post('/chat', async (req, res) => {
   }
 });
 
-app.get('/stream-chat', async (req, res) => {
+app.get('/stream-chat/:room_id', async (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream'); // Para indicar que la respuesta es un stream de eventos
-  const userMessage = req.query.message; 
+  const roomId = req.params.room_id;
+  const systemMessage = { role: 'system', content: 'Eres un experto programador, conoces todos los lenguage de programacion existentes y debes ayudar a otros programadores a resolver sus dudas' }
+  const userMessage = { role: 'user', content: req.query.message };
+
+  // Obteniendo mensajes de la base de datos
+  let messages = [systemMessage];
+  const getMessages = new Promise((resolve, reject) => {
+    db.all(`SELECT role, message as content FROM messages WHERE room_id = ?`, roomId, (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(rows);
+    });
+  });
+  messages = messages.concat(await getMessages);
+
+  // Agregando el mensaje de usuario en la base de datos:
+  messages.push(userMessage);
+  db.run(`INSERT INTO messages (room_id, role, message) VALUES (?, ?, ?)`, [roomId, 'user', userMessage.content], (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  // console.log(messages);
 
   try {
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       stream: true, // Chat GPT en modo stream
-      messages: [
-        { role: 'system', content: 'Eres un experto programador, conoces todos los lenguage de programacion existentes y debes ayudar a otros programadores a resolver sus dudas' }, 
-        { role: 'user', content: userMessage }
-      ],
+      messages: messages,
     });
 
+    let fullAssitantResponse = '';
     // Iteramos sobre la respuesta de OpenAI
     for await (const chunk of response) {
       if (chunk.choices[0].finish_reason === 'stop') { // Cuando Chat GPT termina de responder
         res.write('event: end-chat\n'); // Evento tipo end
         res.write('data: [CHAT-FINALIZADO]\n\n'); // Enviamos el mensaje al cliente
         res.end();
+
+        // Agregando respuesta del asistente a la base de datos
+        db.run(`INSERT INTO messages (room_id, role, message) VALUES (?, ?, ?)`, [roomId, 'assistant', fullAssitantResponse], (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+
+        // console.log(fullAssitantResponse);
         return;
       }
       else {
         const assistantReply = encodeURIComponent(chunk.choices[0].delta.content);
+        fullAssitantResponse += chunk.choices[0].delta.content;
 
         res.write('event: response-chat\n'); // Evento tipo token
         res.write(`data: ${assistantReply}\n\n`); // Enviamos el mensaje al cliente
